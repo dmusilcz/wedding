096d00297bfae96edf20e88508c12c0460a88a20
David Musil <dmusilcz@gmail.com>
2025-04-19 00:23:08 +0200

    Updates

--- js/main.js
+++ js/main.js
@@ -6,8 +6,7 @@
 
 		$(document).click(function (e) {
 	    var container = $("#fh5co-offcanvas, .js-fh5co-nav-toggle");
-	    if (!container.is(e.target) && container.has(e.target).length === 0) {
-
+	    if (!container.is(e.target) && container.has(e.target).length === 0 || e.target.className === 'navlink') {
 	    	if ( $('body').hasClass('offcanvas') ) {
 
     			$('body').removeClass('offcanvas');
